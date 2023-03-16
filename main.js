65f7ddf844c63550ae24d215fbdfc54f0cabcbe0
SwartzJonah <swartzjonah@gmail.com>
2023-03-16 16:49:50 -0400

    change between celsius and fahrenhiet

--- main.js
+++ main.js
@@ -18,104 +18,133 @@ let spacing = '\xa0\xa0\xa0\xa0\xa0';
 
 async function getWeather(city) {
     const contentDiv = document.querySelector("#content");
+    contentDiv.replaceChildren();
     const newcity = city.toString()
     console.log(newcity)
-    try{
-    const response = await
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + newcity + '&APPID=0aa2d1d8bde34a199d429347227fd53b');
-    const data = await response.json();
-    contentDiv.replaceChildren();   
-    const place = data.name;
+    try {
+        const response = await
+            fetch('https://api.openweathermap.org/data/2.5/weather?q=' + newcity + '&APPID=0aa2d1d8bde34a199d429347227fd53b');
+        const data = await response.json();
+        const place = data.name;
 
-    const weatherName = data.weather[0].main;
+        const weatherName = data.weather[0].main;
 
-    const weatherDescription = data.weather[0].description;
+        const weatherDescription = data.weather[0].description;
 
 
-    const kelvinTemperature = data.main.temp;
-    const kelvinMax = data.main.temp_max;
-    const kelvinMin = data.main.temp_min
+        const kelvinTemperature = data.main.temp;
+        const kelvinMax = data.main.temp_max;
+        const kelvinMin = data.main.temp_min
 
-    const celsiusTemperature = changeToC(kelvinTemperature);
-    const celsiusMax = changeToC(kelvinMax);
-    const celsiusMin = changeToC(kelvinMin);
+        const celsiusTemperature = changeToC(kelvinTemperature);
+        const celsiusMax = changeToC(kelvinMax);
+        const celsiusMin = changeToC(kelvinMin);
 
-    const fahrenheitTemperature = changeToF(celsiusTemperature);
-    const fahrenheitMax = changeToF(celsiusMax);
-    const fahrenheitMin = changeToF(celsiusMin);
+        const fahrenheitTemperature = changeToF(celsiusTemperature);
+        const fahrenheitMax = changeToF(celsiusMax);
+        const fahrenheitMin = changeToF(celsiusMin);
 
-    const timezone = data.timezone
-    const humidity = data.main.humidity;
+        const timezone = data.timezone
+        const humidity = data.main.humidity;
 
-    const localWeather = (0,_factories__WEBPACK_IMPORTED_MODULE_0__.locationFactory)(place, weatherName, weatherDescription,
-        celsiusTemperature, celsiusMin, celsiusMax,
-        fahrenheitTemperature, fahrenheitMin, fahrenheitMax,
-        timezone, humidity);
+        const localWeather = (0,_factories__WEBPACK_IMPORTED_MODULE_0__.locationFactory)(place, weatherName, weatherDescription,
+            celsiusTemperature, celsiusMin, celsiusMax,
+            fahrenheitTemperature, fahrenheitMin, fahrenheitMax,
+            timezone, humidity);
 
-    const blockDiv = document.createElement("div");
-    blockDiv.classList.add("blockDiv");
+        const blockDiv = document.createElement("div");
+        blockDiv.classList.add("blockDiv");
 
-    const locationDiv = document.createElement("div");
-    locationDiv.classList.add("locationDiv")
-    locationDiv.textContent = "Location: " + localWeather.location;
-    blockDiv.appendChild(locationDiv)
 
-    const timeDiv = document.createElement("div");
-    timeDiv.classList.add("timeDiv")
-    timeDiv.textContent = "Time: " + localWeather.fullDate.time;
-    blockDiv.appendChild(timeDiv);
-    //switch to date thing
-    const dateDiv = document.createElement("div");
-    dateDiv.classList.add("dateDiv")
-    dateDiv.textContent = "Date: " + localWeather.fullDate.dayOfWeek + ", " + localWeather.fullDate.month + " " + localWeather.fullDate.dayOfMonth ;
-    blockDiv.appendChild(dateDiv)
+        //block one is weather, time and date
+        const blockOneDiv = document.createElement("div");
+        blockOneDiv.classList.add("blockOneDiv")
 
-    const weatherDiv = document.createElement("div");
-    weatherDiv.classList.add("weatherDiv")
-    weatherDiv.textContent = "Weather: " + localWeather.weather + spacing + " Subweather: " + localWeather.weatherDesc;
-    blockDiv.appendChild(weatherDiv)
+        const locationDiv = document.createElement("div");
+        locationDiv.classList.add("locationDiv")
+        locationDiv.textContent = "Location: " + localWeather.location;
+        blockOneDiv.appendChild(locationDiv)
 
-    const cTempDiv = document.createElement("div");
-    cTempDiv.classList.add("cTempDiv")
+        const timeDiv = document.createElement("div");
+        timeDiv.classList.add("timeDiv")
+        timeDiv.textContent = "Time: " + localWeather.fullDate.time;
+        blockOneDiv.appendChild(timeDiv);
 
-    const cMainDiv = document.createElement("div")
-    cMainDiv.classList.add("cMainDiv")
-    cMainDiv.textContent = "Temperature: " + localWeather.celcTemp + "°C";
-    cTempDiv.appendChild(cMainDiv);
+        const dateDiv = document.createElement("div");
+        dateDiv.classList.add("dateDiv")
+        dateDiv.textContent = "Date: " + localWeather.fullDate.dayOfWeek + ", " + localWeather.fullDate.month + " " + localWeather.fullDate.dayOfMonth;
+        blockOneDiv.appendChild(dateDiv)
 
-    const cSubDiv = document.createElement("div")
-    cSubDiv.classList.add("cSubDiv")
-    cSubDiv.textContent = "Max: " + localWeather.celcMax + "°C" + spacing + "Min: " + localWeather.celcMin + "°C";
-    cTempDiv.appendChild(cSubDiv);
-    blockDiv.appendChild(cTempDiv)
+        blockDiv.appendChild(blockOneDiv)
 
-    const fTempDiv = document.createElement("div");
-    fTempDiv.classList.add("fTempDiv")
+        //block two div is weather and humidity
 
-    const fMainDiv = document.createElement("div")
-    fMainDiv.classList.add("fMainDiv")
-    fMainDiv.textContent = "Temperature: " + localWeather.fahTemp + "°F";
-    fTempDiv.appendChild(fMainDiv);
+        const blockTwoDiv = document.createElement("div");
+        blockTwoDiv.classList.add("blockTwoDiv")
 
-    const fSubDiv = document.createElement("div")
-    fSubDiv.classList.add("fSubDiv")
-    fSubDiv.textContent = "Max: " + localWeather.fahMax + "°F" + spacing + "Min: " + localWeather.fahMin + "°F";
-    fTempDiv.appendChild(fSubDiv);
-    blockDiv.appendChild(fTempDiv)
+        const weatherDiv = document.createElement("div");
+        weatherDiv.classList.add("weatherDiv")
+        weatherDiv.textContent = "Weather: " + localWeather.weather;
+        blockTwoDiv.appendChild(weatherDiv)
 
-    const humidDiv = document.createElement("div");
-    humidDiv.classList.add("humidDiv")
-    humidDiv.textContent = "Humidity: " + localWeather.humid + "%";
-    blockDiv.appendChild(humidDiv)
+        const weatherDescDiv = document.createElement("div");
+        weatherDescDiv.classList.add("weatherDescDiv")
+        weatherDescDiv.textContent = "Subweather: " + localWeather.weatherDesc;
+        blockTwoDiv.appendChild(weatherDescDiv)
 
-    contentDiv.appendChild(blockDiv);
-    console.log(localWeather);
+        const humidDiv = document.createElement("div");
+        humidDiv.classList.add("humidDiv")
+        humidDiv.textContent = "Humidity: " + localWeather.humid + "%";
+        blockTwoDiv.appendChild(humidDiv)
 
-    addSeconds(UTC, timezone * -1);
-    } catch (error){
+        blockDiv.appendChild(blockTwoDiv)
+
+        //block three is the temperatures
+
+        const blockThreeDiv = document.createElement("div");
+        blockThreeDiv.classList.add("blockThreeDiv");
+
+        const cTempDiv = document.createElement("div");
+        cTempDiv.classList.add("cTempDiv")
+
+        const cMainDiv = document.createElement("div")
+        cMainDiv.classList.add("cMainDiv")
+        cMainDiv.textContent = "Temperature: " + localWeather.celcTemp + "°C";
+        cTempDiv.appendChild(cMainDiv);
+
+        const cSubDiv = document.createElement("div")
+        cSubDiv.classList.add("cSubDiv")
+        cSubDiv.textContent = "Max: " + localWeather.celcMax + "°C" + spacing + "Min: " + localWeather.celcMin + "°C";
+        cTempDiv.appendChild(cSubDiv);
+        blockThreeDiv.appendChild(cTempDiv)
+        
+
+        const fTempDiv = document.createElement("div");
+        fTempDiv.classList.add("fTempDiv")
+
+        const fMainDiv = document.createElement("div")
+        fMainDiv.classList.add("fMainDiv")
+        fMainDiv.textContent = "Temperature: " + localWeather.fahTemp + "°F";
+        fTempDiv.appendChild(fMainDiv);
+
+        const fSubDiv = document.createElement("div")
+        fSubDiv.classList.add("fSubDiv")
+        fSubDiv.textContent = "Max: " + localWeather.fahMax + "°F" + spacing + "Min: " + localWeather.fahMin + "°F";
+        fTempDiv.appendChild(fSubDiv);
+    
+        blockThreeDiv.appendChild(fTempDiv)
+        blockDiv.appendChild(blockThreeDiv)
+
+
+        contentDiv.appendChild(blockDiv);
+        console.log(localWeather);
+
+        addSeconds(UTC, timezone * -1);
+    } catch (error) {
+        console.log(error);
         getWeather("New York");
         alert("City does not exist");
-        
+
     }
 }
 
@@ -148,11 +177,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "getUTC": () => (/* binding */ getUTC),
 /* harmony export */   "locationFactory": () => (/* binding */ locationFactory)
 /* harmony export */ });
-function getUTC(){
+function getUTC() {
     const date = new Date();
     const dateOffset = date.getTimezoneOffset() * 60;
     const UTC = addSeconds(date, dateOffset);
-    
+
     return UTC;
 }
 
@@ -164,11 +193,11 @@ const locationFactory = (
     timezone, humid
 ) => {
     const UTC = getUTC();
-    
+
     let time = addSeconds(UTC, timezone);
     ;
     let brokenDate = time.toString();
-    
+
     let fullDate = dateFactory(brokenDate)
     //send in full date
     console.log(fullDate);
@@ -257,7 +286,7 @@ const dateFactory = (date) => {
 }
 
 function addSeconds(date, seconds) {
-    
+
     date.setSeconds(date.getSeconds() + seconds);
 
     return date;
@@ -326,12 +355,27 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_weather__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 
-const date = new Date();
-const dateOffset = date.getTimezoneOffset() * 60;
-const UTC = addSeconds(date, dateOffset);
-let spacing = '\xa0\xa0\xa0\xa0\xa0';
 
 const searchForm = document.querySelector("#searchForm");
+const changeTempBtn = document.querySelector("#changeTempBtn");
+let active = "C";
+
+changeTempBtn.addEventListener("click", (event) => {
+    const fDiv = document.querySelector(".fTempDiv");
+    const cDiv = document.querySelector(".cTempDiv");
+    event.preventDefault();
+    if (active === "C") {
+        fDiv.style.display = "table";
+        cDiv.style.display = 'none';
+        console.log('C')
+        active = "F"
+    } else if (active === "F"){
+        cDiv.style.display = "table";
+        fDiv.style.display = 'none';
+        console.log('F')
+        active = "C"
+    }
+})
 
 searchForm.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -341,13 +385,8 @@ searchForm.addEventListener("submit", (event) => {
 })
 
 
-function addSeconds(date, seconds) {
-    date.setSeconds(date.getSeconds() + seconds);
-    return date;
-}
 
-
-(0,_modules_weather__WEBPACK_IMPORTED_MODULE_0__.getWeather)("New York");
+;(0,_modules_weather__WEBPACK_IMPORTED_MODULE_0__.getWeather)("New York");
 })();
 
 /******/ })()
